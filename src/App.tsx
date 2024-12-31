f4f9f94c189a6f9e1c638e89a9adfea96ae20ba2
Tailan Patrick <tailanpatrick06@gmail.com>
2024-12-31 04:36:28 -0300

    configured toasts in App.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+
 import Globalstyles from "./styles/globalstyles";
 import Header from "./components/Header/index";
 import { AppRoutes } from './routes/index'
@@ -8,9 +10,10 @@ import { AppRoutes } from './routes/index'
 function App() {
   return (
     <BrowserRouter>
-      <Globalstyles/>
       <Header />
       <AppRoutes/>
+      <Globalstyles/>
+      <ToastContainer autoClose={ 3000 } className="toast-container"/>
     </BrowserRouter>
   );
 }
