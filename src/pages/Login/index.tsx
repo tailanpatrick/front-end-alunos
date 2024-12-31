d2d784ad5959f122514dbc43f4f34aaeed021f06
Tailan Patrick <tailanpatrick06@gmail.com>
2024-12-31 04:40:36 -0300

    testing axios

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from '../../services/axios';
+
 
 import { Container } from "../../styles/globalstyles";
-import { Title, Paragraph } from './styled'
+import { Title, Paragraph } from './styled';
+import { toast } from 'react-toastify';
 
 export default function Login() {
+
+  useEffect(()=> {
+    async function getData(){
+      const response = await axios.get('/users');
+      console.log(response.data)
+    }
+    getData();
+  },[]);
+
+
   return (
     <Container>
       <Title isRed={true}>
