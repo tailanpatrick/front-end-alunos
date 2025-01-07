a6b67a262b46541ecb6ab269be6185fd468fac1b
Tailan Patrick <tailanpatrick06@gmail.com>
2025-01-07 10:16:16 -0300

    Test of react-redux o onclick of button change text on header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { Link } from 'react-router-dom';
-import { FaHome, FaSignInAlt, FaUser, FaUserAlt } from 'react-icons/fa';
+import { useSelector } from "react-redux";
+
+import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
 
 
 import { Nav } from './styled'
 
 export default function Header() {
+  const botaoClicado = useSelector((state: any) => state.button.clicked)
+
   return <Nav>
     <Link to="/"><FaHome size={24}/></Link>
     <Link to="/login"><FaUserAlt size={24}/></Link>
     <Link to="/asasd"><FaSignInAlt size={24}/></Link>
+    {(botaoClicado ?? false) ? 'Clicado' : 'Não clicado'}
   </Nav>
 }
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,12 +1,15 @@
 import React, { useEffect } from "react";
-import axios from '../../services/axios';
+import { useDispatch } from 'react-redux'
 
+import axios from '../../services/axios';
 
 import { Container } from "../../styles/globalstyles";
 import { Title, Paragraph } from './styled';
 import { toast } from 'react-toastify';
+import { buttonClicked } from "../../store/slices/buttonSlice";
 
 export default function Login() {
+  const dispatch = useDispatch()
 
   useEffect(()=> {
     async function getData(){
@@ -17,6 +20,12 @@ export default function Login() {
   },[]);
 
 
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    dispatch(buttonClicked());
+  }
+
   return (
     <Container>
       <Title isRed={true}>
@@ -24,7 +33,7 @@ export default function Login() {
         <small>Oie</small>
       </Title>
       <Paragraph>Lorem ipsum dolor sit amet consectetur .</Paragraph>
-      <button>Enviar</button>
+      <button onClick={handleClick}>Enviar</button>
     </Container>
   );
 }
