b2c8a0d816126554e0f5a95f4c3e4d50c33ecd63
Tailan Patrick <tailanpatrick06@gmail.com>
2025-01-07 10:13:24 -0300

    Wrapping the application with the Provider passing store

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import React from "react";
-import { BrowserRouter } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { Provider } from "react-redux";
 
+import store from './store/store'
 import Globalstyles from "./styles/globalstyles";
 import Header from "./components/Header/index";
-import { AppRoutes } from './routes/index'
-
+import { AppRoutes } from "./routes/index";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <AppRoutes/>
-      <Globalstyles/>
-      <ToastContainer autoClose={ 3000 } className="toast-container"/>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <AppRoutes />
+        <Globalstyles />
+        <ToastContainer autoClose={3000} className="toast-container" />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
