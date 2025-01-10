8e515d9ce08741839ff9abe7d1180449fdf23bfd
Tailan Patrick <tailanpatrick06@gmail.com>
2025-01-10 09:59:08 -0300

    removido react-toasts de mensagem e Math.random no Promise

--- src/store/saga/sagas.ts
+++ src/store/saga/sagas.ts
@@ -7,19 +7,18 @@ import { toast } from 'react-toastify'
 const request = () =>
   new Promise<void>((resolve, reject) => {
     setTimeout(() => {
-      if (Math.random() > 0.5) resolve(); // Simula sucesso
-      else reject(new Error('Erro simulado')); // Simula falha
-    }, 2000);
+      resolve(); // Simula sucesso
+    }, 600);
   });
 
 function* handleButtonClick() {
   try {
     yield call(request);
     yield put(buttonClickedSuccess());
-    toast.success('Sucesso')
+
   } catch (err) {
     yield put(buttonClickedFailure());
-    toast.error('Deu erro')
+
   }
 }
 
