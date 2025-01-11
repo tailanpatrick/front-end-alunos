b2d8823c750c924c3e06662588950c91e16e9a4b
Tailan Patrick <tailanpatrick06@gmail.com>
2025-01-11 11:32:55 -0300

    create Student page

---
+++ src/pages/Student/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "../../styles/globalstyles";
+
+
+export default function Student() {
+
+  return (
+    <Container>
+      <h1>Student</h1>
+    </Container>
+  );
+}
