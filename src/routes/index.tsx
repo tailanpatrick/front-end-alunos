bfb6c121cd994318fc749c492f45fe038de9d5f1
Tailan Patrick <tailanpatrick06@gmail.com>
2025-01-11 11:35:41 -0300

    Once application routes are configured, routes can be opened or closed

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,33 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+
 import MyRoute from './MyRoute'; // Importando MyRoute
+
 import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Student from "../pages/Student";
+import Students from "../pages/Students";
+import Photos from "../pages/Photos";
 import Page404 from "../pages/Page404";
 
 export const AppRoutes = () => {
   return (
     <Routes>
 
-      <Route path="/" element={<MyRoute Component={Login} isClosed={false} />} />
+      <Route path="/" element={<MyRoute Component={Students} isClosed={true} />} />
+
+      <Route path="/student/:id/edit" element={<MyRoute Component={Student} isClosed={true} />} />
+
+      <Route path="/student/" element={<MyRoute Component={Student} isClosed={true} />} />
+
+      <Route path="/photos/:id" element={<MyRoute Component={Photos} isClosed={true} />} />
+
+      <Route path="/register" element={<MyRoute Component={Register} isClosed={false} />} />
+
+      <Route path="/login" element={<MyRoute Component={Login} isClosed={false} />} />
+
       <Route path="*" element={<MyRoute Component={Page404} isClosed={false} />} />
+
     </Routes>
   );
 };
