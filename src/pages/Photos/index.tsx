b055be60f0572a2e9cdf6e7974d2d254c64a3e91
Tailan Patrick <tailanpatrick06@gmail.com>
2025-01-11 11:32:31 -0300

    create Photos page

---
+++ src/pages/Photos/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "../../styles/globalstyles";
+
+
+export default function Photos() {
+
+  return (
+    <Container>
+      <h1>Photos</h1>
+    </Container>
+  );
+}
