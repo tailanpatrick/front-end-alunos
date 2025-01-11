8cbcc6d8d9ad9c2ea743e8ebac2c6288a62a3d51
Tailan Patrick <tailanpatrick06@gmail.com>
2025-01-11 11:35:08 -0300

    temporarily removed unnecessary codes from login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,40 +1,12 @@
-import React, { useEffect } from "react";
-import { useDispatch } from 'react-redux'
-
-import axios from '../../services/axios';
-
+import React from "react";
 import { Container } from "../../styles/globalstyles";
-import { Title, Paragraph } from './styled';
-import { toast } from 'react-toastify';
 
-import { buttonClickedRequest } from "../../store/reducers/buttonRequest";
 
 export default function Login() {
-  const dispatch = useDispatch()
-
-  useEffect(()=> {
-    async function getData(){
-      const response = await axios.get('/users');
-      console.log(response.data)
-    }
-    getData();
-  },[]);
-
-
-  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-
-    dispatch(buttonClickedRequest());
-  }
 
   return (
     <Container>
-      <Title isRed={true}>
-        Login
-        <small>Oie</small>
-      </Title>
-      <Paragraph>Lorem ipsum dolor sit amet consectetur .</Paragraph>
-      <button onClick={handleClick}>Enviar</button>
+      <h1>Login</h1>
     </Container>
   );
 }
