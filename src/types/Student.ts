20c14e7632c20ef91995180697cbd1d694da5c22
Tailan Patrick <tailanpatrick06@gmail.com>
2025-01-14 06:26:02 -0300

    create type Student

---
+++ src/types/Student.ts
@@ -0,0 +1,19 @@
+export type Student =  {
+  id: string;
+  name: string;
+  surname: string;
+  email: string;
+  age: Number;
+  weight: Number;
+  height: Number;
+  photo?: Photo;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+type Photo = {
+  id: string;
+  originalName: string;
+  fileName: string;
+  filePath: string;
+}
