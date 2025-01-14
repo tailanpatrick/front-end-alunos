a90f63b7a2af4794d81d45356a937f4b0a4e90f7
Tailan Patrick <tailanpatrick06@gmail.com>
2025-01-14 06:29:11 -0300

    creating the student page with your styles

--- src/pages/Students/index.tsx
+++ src/pages/Students/index.tsx
@@ -1,12 +1,84 @@
-import React from "react";
-import { Container } from "../../styles/globalstyles";
+import React, { useEffect, useState } from "react";
+import axios from "../../services/axios";
+import { Student } from "../../types/Student";
+import { get } from "lodash";
+import { Link } from "react-router-dom";
 
+import { FaUserCircle, FaEdit, FaWindowClose } from "react-icons/fa";
+import { Container } from "../../styles/globalstyles";
+import { ProfilePicture, StudentActions, StudentContainer } from "./styled";
 
 export default function Students() {
+  const [students, setStudents] = useState<Student[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        const response = await axios.get("/students");
+        setStudents(response.data);
+        setError(null);
+      } catch (err: any) {
+        console.error("Erro ao buscar os estudantes", err);
+        setError("Não foi possível carregar a lista de estudantes");
+      } finally {
+        setLoading(false);
+      }
+    }
+    getData();
+  }, []);
+
+  if (loading) {
+    return (
+      <Container>
+        <h1>Carregando...</h1>
+      </Container>
+    );
+  }
+
+  if (error) {
+    return (
+      <Container>
+        <h1>Erro</h1>
+        <p>{error}</p>
+      </Container>
+    );
+  }
 
   return (
     <Container>
-      <h1>Students</h1>
+      <h1>Alunos</h1>
+
+      <StudentContainer>
+        {students.map((student) => (
+          <div key={student.id}>
+            {get(student, "photo.filePath", false) ? (
+              <ProfilePicture>
+                <img
+                  src={student.photo?.filePath}
+                  alt={student.photo?.fileName}
+                />
+              </ProfilePicture>
+            ) : (
+              <FaUserCircle size={36} />
+            )}
+
+            <span>{student.name}</span>
+            <span>{student.email}</span>
+
+            <StudentActions>
+              <Link to={`/student/${student.id}/edit`}>
+                <FaEdit size={19} />
+              </Link>
+
+              <Link to={`/student/${student.id}/delete`}>
+                <FaWindowClose size={20} />
+              </Link>
+            </StudentActions>
+          </div>
+        ))}
+      </StudentContainer>
     </Container>
   );
 }
--- src/pages/Students/styled.ts
+++ src/pages/Students/styled.ts
@@ -15,3 +15,32 @@ export const Title = styled.h1<TitleProps>`
 export const Paragraph = styled.p`
   font-size: 18px;
 `;
+
+export const StudentContainer = styled.div`
+  margin-top: 20px;
+
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    padding: 5px 0;
+  }
+
+  div + div {
+    border-top: solid 1px lightgray;
+  }
+`;
+
+export const ProfilePicture = styled.div`
+  img {
+    width: 36px;
+    height: 36px;
+    border-radius: 50%;
+  }
+`;
+
+export const StudentActions = styled.div`
+  gap: 25px;
+`;
+
+
