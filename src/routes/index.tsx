de19da895c8249c30924ae25c51f77fc8218b79a
Tailan Patrick <tailanpatrick06@gmail.com>
2025-01-16 13:51:50 -0300

    refactoring routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-import MyRoute from './MyRoute'; // Importando MyRoute
+import MyRoute from "./MyRoute";
 
 import Login from "../pages/Login";
 import Register from "../pages/Register";
@@ -10,24 +10,19 @@ import Students from "../pages/Students";
 import Photos from "../pages/Photos";
 import Page404 from "../pages/Page404";
 
-export const AppRoutes = () => {
+export const AppRoutes: React.FC = () => {
   return (
     <Routes>
-
-      <Route path="/" element={<MyRoute Component={Students} isClosed={false} />} />
-
-      <Route path="/student/:id/edit" element={<MyRoute Component={Student} isClosed={true} />} />
-
-      <Route path="/student/" element={<MyRoute Component={Student} isClosed={true} />} />
-
-      <Route path="/photos/:id" element={<MyRoute Component={Photos} isClosed={true} />} />
-
-      <Route path="/register" element={<MyRoute Component={Register} isClosed={false} />} />
-
-      <Route path="/login" element={<MyRoute Component={Login} isClosed={false} />} />
-
-      <Route path="*" element={<MyRoute Component={Page404} isClosed={false} />} />
-
+      <Route path="/" element={<MyRoute Component={Students} isClosed />} />
+      <Route
+        path="/student/:id/edit"
+        element={<MyRoute Component={Student} isClosed />}
+      />
+      <Route path="/student/" element={<MyRoute Component={Student} isClosed />} />
+      <Route path="/photos/:id" element={<MyRoute Component={Photos} isClosed />} />
+      <Route path="/register" element={<MyRoute Component={Register} />} />
+      <Route path="/login" element={<MyRoute Component={Login} />} />
+      <Route path="*" element={<MyRoute Component={Page404} />} />
     </Routes>
   );
 };
