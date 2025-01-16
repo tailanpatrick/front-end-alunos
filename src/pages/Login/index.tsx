3322871c75890f0cb546deaca8e4d09e871f7e12
Tailan Patrick <tailanpatrick06@gmail.com>
2025-01-16 13:50:22 -0300

    created and styled login component

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,12 +1,64 @@
-import React from "react";
-import { Container } from "../../styles/globalstyles";
+// src/components/Login/Login.tsx
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { isEmail } from 'validator';
+import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
 
+import { actionLoginRequest } from '../../store/reducers/loginRequest';
+import { Container } from '../../styles/globalstyles';
+import { Form } from './styled';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    let formErrors = false;
+
+    if (!isEmail(email)) {
+      formErrors = true;
+      toast.error('Email inválido');
+    }
+
+    if (password.length < 6 || password.length > 50) {
+      formErrors = true;
+      toast.error('Senha deve ter entre 6 e 50 caracteres');
+    }
+
+    if (formErrors) return;
+
+
+    setTimeout(()=> {
+
+      navigate('/');
+    }, 1000)
+
+    dispatch(actionLoginRequest({ email, password }));
+  };
 
   return (
     <Container>
       <h1>Login</h1>
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Seu email"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Sua senha"
+        />
+        <button type="submit">Acessar</button>
+      </Form>
     </Container>
   );
 }
