e1255476f50837254b7ec44adbae9fc5b4018dba
Tailan Patrick <tailanpatrick06@gmail.com>
2025-01-17 15:58:03 -0300

    configured Axios to include authorization token in the next requests after user login

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,5 +1,27 @@
 import axios from 'axios';
 
-export default axios.create({
-  baseURL: 'http://localhost:3000/'
-})
+const api = axios.create({
+  baseURL: 'http://localhost:3000/',
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const authData = localStorage.getItem('authData');
+    if (authData) {
+      try {
+        const { token } = JSON.parse(authData);
+        if (token) {
+          config.headers['Authorization'] = `Bearer ${token}`;
+        }
+      } catch (error) {
+        console.error('Erro ao analisar authData do localStorage:', error);
+      }
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
