b7183979a4de9c747488ca7280d941ed4f9e2d00
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-01-21 07:19:17 -0300

    implementing system logout and updating with more icons

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,25 +1,41 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
+import { Link, useNavigate } from 'react-router-dom';
+import { FaHome, FaSignInAlt, FaUserAlt, FaCircle, FaPowerOff } from 'react-icons/fa';
 
-import { actionLoginSuccess } from '../../store/reducers/loginSuccess';
-import { RootState } from '../../store/store';
 import { Nav } from './styled';
 
 export default function Header() {
+  const authData = JSON.parse(localStorage.getItem("authData") || "{}");
+  const { user } = authData;
+  const navigate = useNavigate();
+
+  const handleLogout = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    localStorage.removeItem("authData");
+    navigate('/login')
+  }
 
   return (
     <Nav>
-      <Link to="/">
-        <FaHome size={24} />
+      <Link to="/" title="Página Inicial">
+        <FaHome size={24}/>
       </Link>
-      <Link to="/register">
+      <Link to="/register" title={user?.id ? 'Editar dados de usuário' : 'Fazer registro de usuário'}>
         <FaUserAlt size={24} />
       </Link>
-      <Link to="/login">
+
+      {user?.id ? (
+        <Link onClick={handleLogout} to="/logout" title={`Desconectar de ${user?.name}`} >
+        <FaPowerOff size={24} />
+      </Link>
+      ):(
+        <Link to="/login" title="Fazer Login">
         <FaSignInAlt size={24} />
       </Link>
+      )}
+
+      {user?.id && <FaCircle size={24} color="#66ff33" title={`Logado como ${user?.name}`} style={{cursor: 'pointer'}}/>}
+
     </Nav>
   );
 }
