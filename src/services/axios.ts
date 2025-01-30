3cb83ba4fabe8186ec75d39c6eef1b53dc545889
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-01-30 05:19:23 -0300

    changed the url of axios for the api hosted

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.100.175:3000/',
+  baseURL: 'https://rest-api-alunos.vercel.app',
 });
 
 api.interceptors.request.use(
