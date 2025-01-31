3b64dc9e7d23e3dc6b5b4bd7d605fba68108b555
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-01-31 13:01:03 -0300

    styling page for uploading student profile photo

--- src/pages/Photos/styled.ts
+++ src/pages/Photos/styled.ts
@@ -1,17 +1,40 @@
 import styled from 'styled-components';
+import * as colors from '../../config/colors';
 
 interface TitleProps {
   isRed?: boolean;
 }
 
 export const Title = styled.h1<TitleProps>`
-  background: green;
-
-  small {
-    font-size: 8px;
+margin-left: 25px;
+  @media (max-width: 620px){
+    margin-left: 0;
+    text-align: center;
   }
 `;
 
-export const Paragraph = styled.p`
-  font-size: 18px;
-`;
+export const Form = styled.form`
+
+  label {
+    width: 180px;
+    height: 180px;
+    display: flex;
+    background: #eee;
+    border: 5px dashed ${colors.primaryColor};
+    border-radius: 50%;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    overflow: hidden;
+    margin: 30px auto;
+  }
+
+  input {
+    display: none;
+  }
+
+  img {
+    width: 180px;
+    height: 180px;
+  }
+`
